4bc375c1aecc9cbef561d673140debb5f94fbc76
BramFam96 <72650406+BramFam96@users.noreply.github.com>
2021-08-05 02:02:28 -0500

    created forecast cards

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -2,7 +2,9 @@ import Location from './Location'
 export default function Heading({ cityName, countryName }) {
 	return (
 		<section className='heading_section'>
-			<Location cityName={cityName} countryName={countryName} />
+			<h1>
+				<Location cityName={cityName} countryName={countryName} />
+			</h1>
 			<h2>Currently:</h2>
 		</section>
 	)
--- src/components/Heading/Location.jsx
+++ src/components/Heading/Location.jsx
@@ -1,8 +1,8 @@
 export default function Location({ cityName, countryName }) {
 	return (
-		<div>
+		<>
 			<h1 className='city'>{cityName}</h1>
 			<sup className='city_sup accent-bg'>{countryName}</sup>
-		</div>
+		</>
 	)
 }
